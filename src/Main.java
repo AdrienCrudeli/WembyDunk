84601b1945918d36e295168afd354468b9e7d849
adrien <adrien@localhost>
2025-03-24 05:54:49 +0100

    refactoriser encore

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		int[] a = new int[2] ;
 		a[0]=1;
 		a[1]=1;
-		System.out.println(v.toString(v.addition(a)));
+;
 
 		//affichage basique en x,y, notez qu'il faudra faire de la composition comme le tuto, on s'en sert juste pour testes
 		JFrame window = new JFrame(); //on crée la fenêtre d'affichage
