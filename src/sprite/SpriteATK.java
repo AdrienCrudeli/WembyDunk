b6fcf46f083c0abc2bfede227b7fc292e312730a
elzbi <elzbi@172.30.35.10>
2025-04-11 10:19:56 +0200

    sprites

---
+++ src/sprite/SpriteATK.java
@@ -0,0 +1,22 @@
+package sprite;
+   
+
+import Test_codeur.GamePanel;
+import Test_codeur.KeyHandler;
+import player.PlayerMoovset;
+
+public class SpriteATK extends PlayerMoovset {
+	
+	GamePanel gp;
+	KeyHandler keyH;
+	public SpriteATK(GamePanel gp, KeyHandler keyH) {
+		this.gp = gp;
+		this.keyH = keyH;
+		}
+}
+public void update() {
+	
+}
+public void draw() {
+	
+}
